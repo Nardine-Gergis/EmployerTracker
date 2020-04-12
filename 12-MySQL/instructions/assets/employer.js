d6c41f87b8a8f5f481a992f297d99e068ffaa300
Nardne-Gergis <nardinegergis@gmail.com>
2020-04-12 17:53:24 -0400

    installed inquirer package

--- 12-MySQL/instructions/assets/employer.js
+++ 12-MySQL/instructions/assets/employer.js
@@ -1,6 +1,30 @@
-var mysql = require ("mysql");
-var inquirer = require("inquirer");
+const mysql = require ("mysql");
+const inquirer = require("inquirer");
 
 
 //connect to the database 
+const connection = mysql.createConnection({
+
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Nodi92179",
+    database: "employee_DB"
+
+});
+
+connection.connect(function(err){
+
+if(err) throw err;
+
+
+});
+
+
 
